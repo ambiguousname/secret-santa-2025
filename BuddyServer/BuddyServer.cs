03392014c63efca2d64d2e3d28f672d35a6d2cb2
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-12-06 10:48:41 -0800

    Move TCP server to its own Task

--- BuddyServer/BuddyServer.cs
+++ BuddyServer/BuddyServer.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Text;
 using Modding;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
 
 namespace BuddyServer
 {
@@ -10,45 +13,59 @@ namespace BuddyServer
     {
         private readonly TcpListener server = new TcpListener(IPAddress.Any, 5121);
 
-        private bool shutdown = false;
-
-        async private void ConnectClient() {
-
+        async private void CreateServer() {
+            server.Start();
             while (true) {
+                Log("Waiting for client...");
                 TcpClient c = await server.AcceptTcpClientAsync();
                 c.ReceiveTimeout = 1000;
                 NetworkStream st = c.GetStream();
+                Log("Client connected!");
+
+                // We can't read or write.
+                if (!(st.CanRead && st.CanWrite)) {
+                    Log("Client stream does not support read or write. Disconnecting...");
+                    c.Close();
+                    continue;
+                }
 
                 string expected = "BuddyClient";
                 byte[] buffer = new byte[expected.Length];
                 await st.ReadAsync(buffer, 0, expected.Length);
 
                 if (Encoding.UTF8.GetString(buffer) != expected) {
+                    Log($"Handshake failed! Got {Encoding.UTF8.GetString(buffer)}");
                     c.Close();
                     continue;
                 }
 
                 await st.WriteAsync(Encoding.UTF8.GetBytes("BuddyServer"), 0, expected.Length);
 
-                while (c.Connected && !shutdown) {
+                Log("Handshake succeeded.");
+
+                while (c.Connected) {
                     // TODO: Send over state data.
-                }
-                if (shutdown) {
-                    return;
                 }
             }
         }
 
+        Task tcpServerTask;
+        CancellationTokenSource cts;
+
         public override void Initialize() {
-            server.Start();
-            ConnectClient();
+            cts = new CancellationTokenSource();
+
+            Log("Starting TCP server...");
+            tcpServerTask = new Task(CreateServer, cts.Token, TaskCreationOptions.LongRunning);
+            tcpServerTask.Start();
         }
 
         public override string GetVersion() => "0.0.1";
 
         public void Unload() {
-            shutdown = true;
+            cts.Cancel();
             server.Stop();
+            Log("Server stopped.");
         }
     }
 }
