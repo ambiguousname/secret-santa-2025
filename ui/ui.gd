7b7521bbbdd58574eff0c22cb18e768b7a5fefa4
ambiguousname <ambiguousemails@proton.me>
2025-12-09 09:01:20 -0800

    Set UI hide functionality

--- ui/ui.gd
+++ ui/ui.gd
@@ -4,3 +4,13 @@ class_name UI extends Control
 
 func update_tcp_status(text : String):
 	tcp_status.text = text;
+
+@onready var adventure : Button = $Buttons/Adventure;
+func _ready() -> void:
+	adventure.pressed.connect(func(): 
+		set_ui_display(false);
+	);
+
+func set_ui_display(vis : bool):
+	self.visible = vis;
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, !vis);
