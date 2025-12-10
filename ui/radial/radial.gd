bb2d32c767120420cf91d7b18ac532b9c302f1c5
ambiguousname <ambiguousemails@proton.me>
2025-12-09 19:36:21 -0800

    Use _draw instead of shader

--- ui/radial/radial.gd
+++ ui/radial/radial.gd
@@ -1,23 +1,31 @@
 @tool
-extends ColorRect
+extends Control
 
+@export var inner_width : float = 2.0:
+	set(v):
+		inner_width = v;
+		queue_redraw();
 @export var amount : float = 1.0:
-	get():
-		return amount;
 	set(v):
 		amount = v;
-		self.material.set_shader_parameter("angle", 2 * PI * amount);
+		queue_redraw();
 
 @export var radial_color : Color = Color.WHITE:
-	get():
-		return radial_color;
 	set(v):
 		radial_color = v;
-		self.material.set_shader_parameter("color", radial_color);
+		queue_redraw();
 
 @export var unfilled_color : Color = Color(0, 0, 0, 0):
-	get():
-		return unfilled_color;
 	set(v):
 		unfilled_color = v;
-		self.material.set_shader_parameter("unfilled_color", unfilled_color);
+		queue_redraw();
+
+@export var subdivisions : int = 75:
+	set(v):
+		subdivisions = v;
+		queue_redraw();
+
+func _draw() -> void:
+	draw_arc(Vector2(size.x/2, size.y/2), size.x/2 - inner_width/2, 0.0, amount * 2 * PI, subdivisions, radial_color, inner_width, false);
+	if amount < 2 * PI || unfilled_color.a == 0:
+		draw_arc(Vector2(size.x/2, size.y/2), size.x/2 - inner_width/2, amount * 2 * PI, 2 * PI, subdivisions, unfilled_color, inner_width, false);
