66efacbf5b3f716e0849f0e74579b32d13ce10b8
ambiguousname <ambiguousemails@proton.me>
2025-12-11 10:15:40 -0800

    Generate racers

--- courses/course.gd
+++ courses/course.gd
@@ -1,5 +1,6 @@
 class_name Course extends Node2D
 
+@onready var racer_start : Node2D = $RacerStart;
 @onready var finish : Area2D = $Finish;
 @onready var kill : Area2D = $KillPlane;
 
@@ -12,7 +13,25 @@ func _ready() -> void:
 			var scene_name = args[scene_arg + 1];
 			
 			if scene_name == get_tree().current_scene.scene_file_path:
-				start_race();
+				setup_race(Stats.new());
 
-func start_race():
-	print("A");
+var racing_bug = preload("uid://bylxu2i2xmwp1");
+var racing_bugs : Array[RacingBug] = [];
+
+func setup_race(player_bug_stats : Stats):
+	racing_bugs.clear();
+	add_racer(player_bug_stats);
+	for i in range(3):
+		add_racer(Stats.new());
+
+func add_racer(stats : Stats):
+	var bug : RacingBug = racing_bug.instantiate();
+	bug.stats = stats;
+	
+	racing_bugs.push_back(bug);
+	racer_start.add_child(bug);
+
+func cleanup_race():
+	for b in racing_bugs:
+		b.queue_free();
+	racing_bugs.clear();
---
+++ courses/racing_bugs/racing_bug.gd
@@ -0,0 +1,3 @@
+class_name RacingBug extends RigidBody2D
+
+var stats : Stats = Stats.new();
