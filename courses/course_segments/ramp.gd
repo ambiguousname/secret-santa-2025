bc390df137d4dd95aa8e7c47500a6fb21a3798c7
ambiguousname <ambiguousemails@proton.me>
2025-12-10 19:02:10 -0800

    Add polygon collision shape/drawing type

--- courses/course_segments/ramp.gd
+++ courses/course_segments/ramp.gd
@@ -1,7 +1,6 @@
 @tool
 extends StaticBody2D
 
-@onready var path : Path2D = $Path2D;
 @onready var polygon : CollisionPolygon2D = $CollisionPolygon2D;
 
 @export var color : Color = Color.WHITE;
@@ -9,14 +8,4 @@ extends StaticBody2D
 var d = queue_redraw;
 
 func _draw() -> void:
-	var points := PackedVector2Array();
-	# Add bottom:
-	points.push_back(Vector2(path.curve.sample(0, 0).x, 500));
-	for i in range(26):
-		var point : Vector2 = path.curve.sample_baked(path.curve.get_baked_length() * i/(25.0));
-		points.push_back(point);
-	
-	points.push_back(Vector2(path.curve.sample(path.curve.point_count, 1).x, 500));
-	polygon.polygon = points;
-	
-	draw_colored_polygon(points, color);
+	draw_colored_polygon(polygon.polygon, color);
