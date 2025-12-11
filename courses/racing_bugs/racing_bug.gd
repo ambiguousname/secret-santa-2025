ae2aa3c07a53a287bf186132fad66f422468a1db
ambiguousname <ambiguousemails@proton.me>
2025-12-11 11:10:19 -0800

    Start on racing AI

--- courses/racing_bugs/racing_bug.gd
+++ courses/racing_bugs/racing_bug.gd
@@ -2,5 +2,34 @@ class_name RacingBug extends RigidBody2D
 
 var stats : Stats = Stats.new();
 
+enum State {
+	RUNNING,
+	CLIMBING,
+	FALLING,
+	SKATING,
+	JUMPING,
+};
+var state : State = State.FALLING;
+
+func _ready() -> void:
+	self.contact_monitor = true;
+	self.max_contacts_reported = 4;
+	self.body_entered.connect(func(body : PhysicsBody2D): 
+		if state != State.FALLING:
+			return;
+		if body is StaticBody2D:
+			if test_move(transform, Vector2.DOWN):
+				state = State.RUNNING;
+			else:
+				print("Climb");
+				state = State.CLIMBING;
+	);
+
 func _physics_process(delta: float) -> void:
-	self.apply_force(Vector2(1, -1) * 200.0);
+	match state:
+		State.RUNNING:
+			if get_contact_count() == 0:
+				state = State.FALLING;
+				return;
+			self.apply_force(Vector2.RIGHT * (stats.running.level + 1) * 500);
+	#self.apply_force(Vector2(1, -1) * 1000.0);
