3df348b69f5d36ce9d03bd1686072d70273d1781
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-12-17 07:49:41 -0800

    Add credits to settings

--- ui/settings_menu.gd
+++ ui/settings_menu.gd
@@ -1,4 +1,8 @@
 class_name SettingsMenu extends Control
 
 @onready var close : Button = $Close;
-@onready var install : InstallServer = $InstallServer;
+@onready var tabs : TabContainer = $TabContainer;
+@onready var install : InstallServer = $TabContainer/InstallServer;
+
+func _ready() -> void:
+	tabs.set_tab_title(0, "Hollow Knight");
