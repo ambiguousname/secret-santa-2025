8b5d7e511686613da51ae35df26a675c7f05ceef
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-12-19 14:43:25 -0800

    Add font size

--- ui/audio.gd
+++ ui/audio.gd
@@ -3,6 +3,7 @@ class_name Audio extends Control
 @onready var master_volume : HSlider = %MasterVolume;
 
 func _ready() -> void:
+	master_volume.value = Settings.get_setting("master_volume", master_volume.value);
 	master_volume.drag_ended.connect(func(changed : bool):
 		if changed:
 			Settings.set_setting("master_volume", master_volume.value);
--- ui/text.gd
+++ ui/text.gd
@@ -1,6 +1,7 @@
 class_name TextSettings extends Control
 
 @onready var font_family : OptionButton = %FontFamily;
+@onready var font_size : SpinBox = %FontSize;
 
 var quixel : FontFile = preload("uid://coucdedhg8x0e");
 
@@ -17,7 +18,18 @@ func _ready() -> void:
 		Settings.save();
 	);
 	var fam : FontFamily = Settings.get_setting("font_family", FontFamily.QUIXEL);
+	font_family.selected = fam;
 	set_family(fam);
+	
+	font_size.value_changed.connect(func(v : float):
+		var val := int(v);
+		set_font_size(val);
+		Settings.set_setting("font_size", val);
+		Settings.save();
+	);
+	var f_size : int = Settings.get_setting("font_size", 16);
+	set_font_size(f_size);
+	font_size.value = f_size;
 
 func set_family(family : FontFamily):
 	var def = ThemeDB.get_default_theme();
@@ -27,3 +39,6 @@ func set_family(family : FontFamily):
 			theme.default_font = quixel;
 		FontFamily.NOTOSANS:
 			theme.default_font = def.default_font;
+
+func set_font_size(v : int):
+	ThemeDB.get_project_theme().default_font_size = int(v);
