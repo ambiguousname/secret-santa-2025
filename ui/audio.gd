77c80ec494a68944ccdc7585acb4dd815b78c276
Tyler K <18091407+ambiguousname@users.noreply.github.com>
2025-12-19 14:20:51 -0800

    Add audio slider

---
+++ ui/audio.gd
@@ -0,0 +1,11 @@
+class_name Audio extends Control
+
+@onready var master_volume : HSlider = %MasterVolume;
+
+func _ready() -> void:
+	master_volume.drag_ended.connect(func(changed : bool):
+		if changed:
+			Settings.set_setting("master_volume", master_volume.value);
+			Settings.save();
+			# TODO: Play sample SFX
+	);
